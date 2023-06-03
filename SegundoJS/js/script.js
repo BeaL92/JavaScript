7996ab9c17f3c58a0c012a413176d79412b03429
BeaL92 <bealoro92@gmail.com>
2023-06-03 13:41:35 +0200

    3/06

--- SegundoJS/js/script.js
+++ SegundoJS/js/script.js
@@ -1,6 +1,8 @@
 /*
 1. Operadores de comparación
 2. Operadores Logicos - Y / O
+3. Operadores ternarios
+
 
 */
 
@@ -29,3 +31,25 @@ document.getElementById("comp6").innerHTML=resultado;
 let z="2"; // tipo de cadena de texto
 resultado=(x===z);
 document.getElementById("comp7").innerHTML=resultado;
+
+//Operadores Lógicos
+//Operador Y representados por &&
+resultado=(x==y) && (x===z);// false    false-> false
+document.getElementById("comp8").innerHTML=resultado;
+resultado=(x==y) && (x==z);// false    true-> false
+document.getElementById("comp9").innerHTML=resultado;
+resultado=(x<y) && (x==z);// true    true-> true
+document.getElementById("comp10").innerHTML=resultado;
+//operador lógico OR y se representa con dos barras ||
+resultado=(x==y) || (x===z);// false    false-> false
+document.getElementById("comp11").innerHTML=resultado;
+resultado=(x==y) || (x==z);// false    true-> true
+document.getElementById("comp12").innerHTML=resultado;
+resultado=(x<y) || (x==z);// true    true-> true
+document.getElementById("comp13").innerHTML=resultado;
+//Operadores ternario -> evalua una condición y si se cumple hace algo
+// sino se cumple hace otra cosa
+// estructura-> expresión ? valor o ejecución si es verdadero : valor o ejecución si es falso
+let muestra=x<y?"X es menor que Y":"X es mayor que Y";
+document.getElementById("muestra").innerHTML=muestra;
+
